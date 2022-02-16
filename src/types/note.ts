5b46601fbe8f7ddc30d2525521d0e52eba5f3605
Dor Avrahami <da19965@gmail.com>
2022-02-16 19:13:46 +0200

    Refactored ./src/types/

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,8 +1,8 @@
 import { Document } from "mongoose"
 
 export interface INote extends Document {
-    starId: string;
-    note: string;
-    publisher: string;
-    repliesTo: string;
+  starId: string;
+  note: string;
+  publisher: string;
+  repliesTo?: string;
 }
