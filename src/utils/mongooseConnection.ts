d727df22aafeba865769db831bdae6d6f0cb96ba
mikim <mikimeerson44@gmail.com>
2022-04-24 15:07:26 +0300

    add env

--- src/utils/mongooseConnection.ts
+++ src/utils/mongooseConnection.ts
@@ -1,4 +1,7 @@
 import { connect } from 'mongoose';
+import { config } from 'dotenv';
+
+config();
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tlorz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
